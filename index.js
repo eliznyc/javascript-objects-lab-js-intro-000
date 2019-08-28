a6421a60bd470934dc31aa2ffe56abd33f13d408
Elizabeth Peters <eliznyc@gmail.com>
2019-08-28 02:47:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return object;
+  return object
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+}
+
+
+
