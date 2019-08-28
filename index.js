448e4d8110acb32702aa32d6d5d7e7d6b3cf6b44
Elizabeth Peters <eliznyc@gmail.com>
2019-08-28 03:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
-}
-
-function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj.key;
   return newObj;
